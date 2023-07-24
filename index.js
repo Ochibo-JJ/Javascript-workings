931ef9e4121a3027c6b98c4478f9741aabfa9d51
Ochibo-JJ <133512040+Ochibo-JJ@users.noreply.github.com>
2023-07-24 23:51:24 +0300

    added a counter program and a few changes to hypotenuse code

--- index.js
+++ index.js
@@ -21,3 +21,21 @@
         c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
         document.getElementById("cLabel").innerHTML = "Hypotenuse is: " + c;
      }
+
+//counter program
+let count = 0;
+document.getElementById("decreaseBtn").onclick = function(){
+   if (count>0){
+   document.getElementById("countLabel").innerHTML = count-=1;
+}else{
+   document.getElementById("countLabel").innerHTML = count;
+}}
+document.getElementById("reset").onclick = function(){
+   document.getElementById("countLabel").innerHTML = count=0;
+}
+document.getElementById("increaseBtn").onclick = function(){
+   document.getElementById("countLabel").innerHTML = count+=1;
+}
+document.getElementById("submitBtn").onclick = function(){
+   count = window.alert("Congratulations! You are " + count + " years old!");
+}
